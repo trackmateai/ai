ecb7a17b16fc5dc6c068c6c22368407e744174b7
trackmateai <136100891+trackmateai@users.noreply.github.com>
2023-06-09 20:40:24 +0200

    Added Edwin to the system
    Nothing else

--- loginScript.js
+++ loginScript.js
@@ -12,6 +12,15 @@ function getAccess(){
 
             alert('Wrong Credential! Please make sure to create an Account');
          }
+    if(username=="edwinnkgorosang779@gmail.com"&&password=="nkgorosang8209"){
+            
+            window.location.href = "html2.html";
+        }
+         else{
+
+            alert('Wrong Credential! Please make sure to create an Account');
+         }
+    
 
 
 
