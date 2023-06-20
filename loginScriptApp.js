0c805bc3d3634b5108bf2470434988810c7100e2
trackmateai <136100891+trackmateai@users.noreply.github.com>
2023-06-20 13:29:33 +0200

    Added pp Script
    Pushed

---
+++ loginScriptApp.js
@@ -0,0 +1,123 @@
+function getAccess(){
+    
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+
+
+    var loginLay= document.getElementById("LoginLay");
+    var mainLay= document.getElementById("MainLay");
+    
+    var email= document.getElementById("tittleMain");
+    
+
+
+
+        if(username=="Masele"&&password=="Mypass"){
+            loginLay.style.display=("none");
+            mainLay.style.display=("block");
+            
+            email.innerHTML=(username);
+        }
+         
+    if(username=="edwinnkgorosang779@gmail.com"&&password=="nkgorosang8209"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="amemodibo@gmail.com"&&password=="Stalin1962"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="OCPHAPOGO"&&password=="Phapogo#87"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="Bethel"&&password=="botho728"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+     if(username=="segotsoreuben@gmail.com"&&password=="Rosinahsegotso87"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="smotswakhumo@gmail.com"&&password=="74420750hope"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="daphneymompedi@gmail.com"&&password=="Incorrect"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="williamramatshwan1@gmail.com"&&password=="Will2023"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+   if(username=="smotswakhumo@gmail.com"&&password=="74420750hope"){
+    loginLay.style.display=("none");
+    mainLay.style.display=("block");
+    
+    email.innerHTML=(username);
+        }
+    if(username=="Stimslo"&&password=="PLayerHater1952"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="lonerankgomo@gmail.com"&&password=="Rankgomo"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="mathesbf@gmail.com"&&password=="S3@bello"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="nkisasametsi@gmail.com"&&password=="Chidodoh"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="niahopetshuma2002@gmail.com"&&password=="75600242Nigel"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="ogaatlale@gmail.com"&&password=="Advo2012"){//Gabriel
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+    if(username=="batshwarijealous@gmail.com"&&password=="Obonetse11"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
+
+
+    
+    
+ }
