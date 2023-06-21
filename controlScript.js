f47a2250d44cb48c2c75436f9b50e482efe3e818
trackmateai <136100891+trackmateai@users.noreply.github.com>
2023-06-21 15:36:53 +0200

    Updated Control script
    Pushed

--- controlScript.js
+++ controlScript.js
@@ -1,4 +1,9 @@
- function control(){
-
+ 
+function feedback(){
     
- }
+    var username = document.getElementById("username").value;
+  
+    if (username=="Masele"){
+        window.location.href = "willPayCard.html";
+    }
+}
