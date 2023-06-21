569d5c0fc6e4bf5256e3d85ac915cc9bf2066c66
trackmateai <136100891+trackmateai@users.noreply.github.com>
2023-06-21 19:20:57 +0200

    Update controlScript.js
    njjknj

--- controlScript.js
+++ controlScript.js
@@ -4,6 +4,6 @@ function feedback(){
     var username = document.getElementById("username").value;
   
     if (username=="Masele"){
-        window.location.href = "infoCard.html";
+        window.location.href = "https://trackmateai.github.io/ai/infoCard.html";
     }
 }
