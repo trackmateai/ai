f152692e484813469c850bac65b59f89deaca35e
trackmateai <136100891+trackmateai@users.noreply.github.com>
2023-06-22 00:35:51 +0200

    Added app files
    Pushed

--- appScript.js
+++ appScript.js
@@ -1,5 +1,151 @@
 
-function test(){
 
-    alert('Good! Works perfect');
+
+
+function openNav(){
+    window.location.href=("appTracking.html");
 }
+
+
+
+
+
+function startTracking(){
+    
+    
+    var trkDv= document.getElementById("trackingDiv");
+    var mapDv= document.getElementById("mapDiv");
+    var homeDv= document.getElementById("home");
+    var infoDv= document.getElementById("myInfo0");
+    var disclaDv= document.getElementById("disclaDiv");
+    var feedDv= document.getElementById("feedback0");
+    
+    var textTrk= document.getElementById("startTracking0");
+    var textInf= document.getElementById("myInfo0");
+    var textHm= document.getElementById("all");
+    var textDisc= document.getElementById("disclaimer0");
+    var textFeed= document.getElementById("feed00");
+    
+    textTrk.style.color=("#ffffff");
+    textInf.style.color=("#374151");
+    textHm.style.color=("#374151");
+    textDisc.style.color=("#374151");
+    textFeed.style.color=("#374151");
+    
+    mapDv.style.display=("none");
+    trkDv.style.display=("block");
+    homeDv.style.display=("none");
+    homeDv.style.display=("none");
+    diclaDiv.style.display=("none");
+    feedDv.style.display=("none");
+}
+
+function home(){
+    
+    
+    var trkDv= document.getElementById("trackingDiv");
+    var mapDv= document.getElementById("mapDiv");
+    var homeDv= document.getElementById("home");
+    var disclaDv= document.getElementById("disclaDiv");
+    var feedDv= document.getElementById("feedback0");
+    
+    var textTrk= document.getElementById("startTracking0");
+    var textInf= document.getElementById("myInfo0");
+    var textHm= document.getElementById("all");
+    var textDisc= document.getElementById("disclaimer0");
+    var textFeed= document.getElementById("feed00");
+    
+    
+    textTrk.style.color=("#374151");
+    textInf.style.color=("#374151");
+    textHm.style.color=("#ffffff");
+    textDisc.style.color=("#374151");
+    textFeed.style.color=("#374151");
+    
+    mapDv.style.display=("none");
+    trkDv.style.display=("none");
+    homeDv.style.display=("block");
+    diclaDiv.style.display=("none");
+    feedDv.style.display=("none");
+}
+
+
+function info(){
+    
+    
+    var trkDv= document.getElementById("trackingDiv");
+    var mapDv= document.getElementById("mapDiv");
+    var homeDv= document.getElementById("home");
+    var disclaDv= document.getElementById("disclaDiv");
+    var feedDv= document.getElementById("feedback0");
+    
+    var textTrk= document.getElementById("startTracking0");
+    var textInf= document.getElementById("myInfo0");
+    var textHm= document.getElementById("all");
+    var textDisc= document.getElementById("disclaimer0");
+    var textFeed= document.getElementById("feed00");
+    
+    
+    textTrk.style.color=("#374151");
+    textInf.style.color=("#ffffff");
+    textHm.style.color=("#374151");
+    textDisc.style.color=("#374151");
+    textFeed.style.color=("#374151");
+    
+    mapDv.style.display=("block");
+    trkDv.style.display=("none");
+    homeDv.style.display=("none");
+    homeDv.style.display=("none");
+    diclaDiv.style.display=("none");
+    feedDv.style.display=("none");
+}
+
+
+function disclaimer(){
+    
+    
+    var trkDv= document.getElementById("trackingDiv");
+    var mapDv= document.getElementById("mapDiv");
+    var homeDv= document.getElementById("home");
+    var disclaDv= document.getElementById("disclaDiv");
+    var feedDv= document.getElementById("feedback0");
+    
+    var textTrk= document.getElementById("startTracking0");
+    var textInf= document.getElementById("myInfo0");
+    var textHm= document.getElementById("all");
+    var textDisc= document.getElementById("disclaimer0");
+    var textFeed= document.getElementById("feed00");
+    
+    textTrk.style.color=("#374151");
+    textInf.style.color=("#374151");
+    textHm.style.color=("#374151");
+    textDisc.style.color=("#ffffff");
+    textFeed.style.color=("#374151");
+    
+    mapDv.style.display=("none");
+    trkDv.style.display=("none");
+    homeDv.style.display=("none");
+    homeDv.style.display=("none");
+    diclaDiv.style.display=("block");
+    
+    var loader= document.getElementById("loader20");
+    loader.style.display=("none");
+    feedDv.style.display=("none");
+}
+
+function feedBack(){
+    
+    var username = document.getElementById("username").value;
+  
+    if (username=="Masele"){
+        window.location.href = "https://trackmateai.github.io/ai/infoCard.html";
+    }
+    
+    
+}
+
+
+
+
+
+
