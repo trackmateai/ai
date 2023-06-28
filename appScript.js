b77d868fd3691195af804a112aa49cea94e23b8a
trackmateai <136100891+trackmateai@users.noreply.github.com>
2023-06-28 22:01:04 +0200

    Update appScript.js

--- appScript.js
+++ appScript.js
@@ -163,7 +163,7 @@ function feedBack(){
 if (username=="segotsoreuben@gmail.com"){
 
        var firstInfo= "IMEI: 356761352132648 <br/>Model: Tecno Pop5 Pro BD4j <br/> Location: Locked(payment required)";
-       var secondInfo= "IMEI: 356761352132648 Brand: Tecno Model BD4j Model Name: Pop 5 Pro Device type: Smartphone Design: Classic Released: January2022 r. DualSIM: YES icon SIM card size: Nano Sim, Nano Sim GSM: icon 850 900 1800 1900 HSDPA: YES icon HSPA+ LTE: YES icon LTE-FDD: 700, 800, 850, 900, 1800, 1900, 2100, 2600 YES icon LTE-TDD: 2300 Display: LCD IPS Color (16M) 720x1600px(6.52inch)269ppi Touch screen: YES") ;
+       var secondInfo= "IMEI: 356761352132648 Brand: Tecno Model BD4j Model Name: Pop 5 Pro Device type: Smartphone Design: Classic Released: January2022 r. DualSIM: YES icon SIM card size: Nano Sim, Nano Sim GSM: icon 850 900 1800 1900 HSDPA: YES icon HSPA+ LTE: YES icon LTE-FDD: 700, 800, 850, 900, 1800, 1900, 2100, 2600 YES icon LTE-TDD: 2300 Display: LCD IPS Color (16M) 720x1600px(6.52inch)269ppi Touch screen: YES" ;
         
     
         mainInfo.style.display=("block");
