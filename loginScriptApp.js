1e2d2b424b31b7a47835ff796be5669a84d5a598
trackmateai <136100891+trackmateai@users.noreply.github.com>
2023-08-08 18:40:53 +0200

    Update loginScriptApp.js

--- loginScriptApp.js
+++ loginScriptApp.js
@@ -363,6 +363,12 @@ if(username=="sales.ashflames@gmail.com"&&password=="Modikwa1987"){
         
         email.innerHTML=(username);
         }
+    f(username=="sirserwalo@gmail.com"&&password=="Onameditse"){
+        loginLay.style.display=("none");
+        mainLay.style.display=("block");
+        
+        email.innerHTML=(username);
+        }
             
     
     
