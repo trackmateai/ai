bc3058147c552feed9baf8a66414b5e30ad950b1
trackmateai <136100891+trackmateai@users.noreply.github.com>
2023-09-04 10:18:37 +0200

    Update appScript.js

--- appScript.js
+++ appScript.js
@@ -199,17 +199,7 @@ function feedBack0(){
 
 if (username=="segotsoreuben@gmail.com"){
        
-       var firstInfo= "STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 353589297923356</font><br/>  Model: Spark 7<br/>Brand: TECNO<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana";
-       var secondInfo= "IMEI: 353589297923356<br/>Brand: Tecno <br/>Model: PR651H <br/>Model Name: Spark 7" ;
-        
-                phoneImageLay.style.display=('block');
-                phoneImage.setAttribute('src', 'https://fdn2.gsmarena.com/vv/bigpic/tecno-spark-7.jpg');
-
-         mainInfo.innerHTML=(firstInfo);
-         moreInfo.innerHTML=(secondInfo);    
-        
-        mainInfo.style.display=("block");
-        moreInfo.style.display=("block");  
+       window.location.href=("benResults35065.html");
     }
     
 if (username=="ashrafprofessor30@gmail.com"){
