0016b62de9bab9fe6ff1fe7a5f30eb1359e7b432
trackmateai <136100891+trackmateai@users.noreply.github.com>
2023-09-18 16:09:40 +0200

    Create signUp.js

---
+++ signUp.js
@@ -0,0 +1,84 @@
+
+function signUpFirebase(){
+    
+      
+      var firebaseConfig = {
+      apiKey: "AIzaSyD09GeejzBofXCWBs_GBj8rDtIEDHd0O98",
+      authDomain: "https://www.google.com",
+      projectId: "masele-33a46",
+      storageBucket: "YOUR_STORAGE_BUCKET",
+      messagingSenderId: "200216185593",
+      appId: "YOUR_APP_ID"
+    };
+    firebase.initializeApp(firebaseConfig);
+
+      
+      
+      var email = document.getElementById("email").value;
+      var password = document.getElementById("password").value;
+
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(function(user) {
+          
+          alert("Sign up successful!");
+          window.location.href=("https://www.bing.com/");
+          
+        })
+        .catch(function(error) {
+          console.log(error.message);
+        });
+    
+}
+
+
+
+
+function signInFirebase(){
+    
+    
+    var firebaseConfig = {
+      apiKey: "AIzaSyD09GeejzBofXCWBs_GBj8rDtIEDHd0O98",
+      authDomain: "https://www.google.com",
+      projectId: "masele-33a46",
+      storageBucket: "YOUR_STORAGE_BUCKET",
+      messagingSenderId: "200216185593",
+      appId: "YOUR_APP_ID"
+    };
+    firebase.initializeApp(firebaseConfig);
+    
+    
+      var email = document.getElementById("username").value;
+      var password = document.getElementById("passwordIn").value;
+
+      firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(function(user) {
+          
+          window.location.href=("https://trackmateai.github.io/ai/appHome2.html");
+          
+        })
+        .catch(function(error) {
+          console.log(error.message);
+        });
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
