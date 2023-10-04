d99d34668765eab8cbd17781343178d1c7862634
trackmateai <136100891+trackmateai@users.noreply.github.com>
2023-10-04 11:15:18 +0200

    Update appScript.js

--- appScript.js
+++ appScript.js
@@ -368,7 +368,7 @@ if (username=="dicemodiagane@gmail.com"){
             phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-xs-new.jpg";
         
         mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 357218090012839</font><br/>  Model: : iPhone Xs (A2097)<br/>Brand: APPLE<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
-        moreInfo.innerHTML=("IMEI: 357218090012839 <br/>Brand: Apple <br/>Model: iPhone XS (A2097) <br/>Model Name: iPhone XS <br/>Carrier/SimLock Info:<a href=('#')> HERE (FREE) <br/>Activation/Coverage Check: <a href='#'>HERE</a> <br/>Find My iPhone: <font color=#00ee00>ON</font> <br/>iCloud Status: Clean <br/>Free Blacklist Check: <a href='#'>HERE</a> ");
+        moreInfo.innerHTML=("IMEI: 357218090012839 <br/>Brand: Apple <br/>Model: iPhone XS (A2097) <br/>Model Name: iPhone XS <br/>Carrier/SimLock Info:<a href=('#')> HERE (FREE)</a> <br/>Activation/Coverage Check: <a href='#'>HERE</a> <br/>Find My iPhone: <font color=#00ee00>ON</font> <br/>iCloud Status: Clean <br/>Free Blacklist Check: <a href='#'>HERE</a> ");
         
         mainInfo.style.display=("block");
         moreInfo.style.display=("block");  
