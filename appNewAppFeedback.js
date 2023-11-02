ce6d08ccb3161651a4e687ec0e629ee2067fe7b8
trackmateai <136100891+trackmateai@users.noreply.github.com>
2023-11-02 13:59:33 +0200

    Update appNewAppFeedback.js

--- appNewAppFeedback.js
+++ appNewAppFeedback.js
@@ -40,7 +40,14 @@ function feedBack(){
             window.location.href=("resultsDice86739.html");
     }
 if (username=="segotsoreuben@gmail.com"){
-window.location.href=("resultsBen35648.html"); 
+            phoneImageLay.style.display=("block");
+            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a03-core.jpg";
+        
+        mainInfo.innerHTML=("STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 356710800479192</font><br/>  Model: Galaxy A03 Core (SM-A032F)<br/>Brand: SAMSUNG<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
+        moreInfo.innerHTML=("Model Info: MOBILE SM-A032F INS<br/>Search Term: 356710800479192<br/>IMEI 1: 350655750479195<br/>Serial Number: <font color=#ff0000>R9ZTB0DXXEE</font><br/>Model Desc: Samsung Galaxy A03 Core<br/>Model Name: SM-A032F<br/>Model Number: SM-A032FLG6INS<br/>Warranty Status: Warranty Active<br/>Estimated Warranty End Date: 16-11-2024<br/>Production location: Korea SEC<br/>Production Date: 16-11-2022<br/>Country: India<br/>Carrier: Factory Unlocked ");
+        
+        mainInfo.style.display=("block");
+        moreInfo.style.display=("block");  
     }
 
  if (username=="mollyjacob1980@gmail.com"){
