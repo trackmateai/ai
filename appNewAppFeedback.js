40a1bfe75b5aa898fd53451c7adad0d624bc8bd5
trackmateai <136100891+trackmateai@users.noreply.github.com>
2023-11-06 11:31:10 +0200

    Update appNewAppFeedback.js

--- appNewAppFeedback.js
+++ appNewAppFeedback.js
@@ -57,10 +57,10 @@ if (username=="segotsoreuben@gmail.com"){
 if (username=="ogaatlale@gmail.com"){
 
             phoneImageLay.style.display=("block");
-            phoneImage.src = "https://www.imei.info/media/t/gsm-cache/g/5/puLfNi-d/tecno-spark-9t.jpg";
+            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a03-core.jpg";
         
-        mainInfo.innerHTML=("STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 352577602074743</font><br/>  Model: Spark 9T<br/>Brand: TECNO<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana");
-        moreInfo.innerHTML=(" Operating System	Android 12<br/>Chipset	MediaTek Helio G37<br/>Number of CPU Cores	4<br/>RAM	4 GB<br/> Width	75.55 mm<br/>Height	164.27 mm<br/>Thickness	8.65 mm<br/> Type	LCD_IPS<br/>Diagonal	6.6 inches<br/>Resolution	720x1600 px<br/>Screen-to-body ratio	84.2 %<br/>Pixel density	267 ppi<br/>Colors	16M  ");
+        mainInfo.innerHTML=("STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 351389785644677</font><br/>  Model: Galaxy A03 Core (SM-A032F)<br/>Brand: Samsung<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana");
+        moreInfo.innerHTML=("Model Info: MOBILE SM-A032F/DS BLACK AFC<br/>Search Term: 351389785644677<br/>IMEI 1: 351389785644677<br/>Serial Number: <font color=#ff0000>R7ST40C1Y7W</font><br/>Model Desc: Samsung Galaxy A03 Core<br/>Model Name: SM-A032F/DS<br/>Model Number: SM-A032FZKDAFC<br/>Warranty Status: Warranty Active<br/>Estimated Warranty End Date: 21-04-2024<br/>Production location: Korea SEC<br/>Production Date: 21-04-2022  ");
         
         mainInfo.style.display=("block");
         moreInfo.style.display=("block");   
