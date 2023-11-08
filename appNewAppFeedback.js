b2da98ea8dfac31fafd87475bfae094585ff5ebd
trackmateai <136100891+trackmateai@users.noreply.github.com>
2023-11-08 11:26:50 +0200

    Update appNewAppFeedback.js

--- appNewAppFeedback.js
+++ appNewAppFeedback.js
@@ -57,10 +57,10 @@ if (username=="segotsoreuben@gmail.com"){
 if (username=="ogaatlale@gmail.com"){
 
             phoneImageLay.style.display=("block");
-            phoneImage.src = "https://www.imei.info/media/t/gsm-cache/X/H/BpkjP1-d/tecno-pop-7-pro.jpg";
+            phoneImage.src = "https://www.imei.info/media/t/gsm-cache/N/u/gg5OOy-d/itel-vision-5.jpg";
         
-        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 356983901231578</font><br/>  Model: Pop 7 Pro<br/>Brand: TECNO<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
-        moreInfo.innerHTML=("Operating System	Android 12<br/>Chipset	Mediatek MT6761V Helio A22<br/>Number of CPU Cores	4<br/>RAM	3 GB<br/>Width	75.5 mm<br/>Height	163.9 mm<br/>Thickness	8.9 mm<br/>Weight	191.0 g<br/> ype	LCD_IPS<br/>Diagonal	6.56 inches<br/>Resolution	720x1600 px<br/>Screen-to-body ratio	83.5 %<br/>Pixel density	267 ppi<br/>Colors	16M<br/>Other display Features	Capacitive, Multi-touch");
+        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 351318810050808</font><br/>  Model: Vision 5<br/>Brand: ITEL<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
+        moreInfo.innerHTML=("Operating System	Android 12 Go Edition<br/>Chipset	UNISOC SC9863A<br/>Number of CPU Cores	4<br/>RAM	3 GB<br/>Internal Memory	64 GB<br/>Memory Card Type	microSDXC<br/>GPU Name	PowerVR GE8322<br/>Type	LCD_IPS<br/>Diagonal	6.6 inches<br/>Resolution	720x1612 px<br/>Screen-to-body ratio	81.2 %<br/>Pixel density	267 ppi<br/>Colors	16M");
         
         mainInfo.style.display=("block");
         moreInfo.style.display=("block");   
