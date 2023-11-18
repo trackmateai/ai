a4fc116d2b53c008bee2f0be1ad95f57fc2ce7f1
trackmateai <136100891+trackmateai@users.noreply.github.com>
2023-11-18 11:48:06 +0200

    Update appNewAppFeedback.js

--- appNewAppFeedback.js
+++ appNewAppFeedback.js
@@ -47,7 +47,14 @@ function feedBack(){
         moreInfo.style.display=("block"); 
     }
 if (username=="segotsoreuben@gmail.com"){
-            window.location.href=("resultsBen35097.html");
+           phoneImageLay.style.display=("block");
+            phoneImage.src = "https://www.imei.info/media/t/gsm-cache/t/O/jNMWHQ-d/tecno-camon-19.jpg";
+        
+        mainInfo.innerHTML=("STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 356042146553427</font><br/>  Model: Camon 19<br/>Brand: TECNO<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
+        moreInfo.innerHTML=(" Operating System	Android 12<br/>Chipset	Mediatek MT6781 Helio G96<br/>Number of CPU Cores	2<br/>RAM	4 GB<br/>Internal Memory	128 GB<br/>Memory Card Type	microSDXC<br/>GPU Name	Mali-G52 MC2<br/>GPU Cores	2<br/>GPU Clock	1000.0 MHz<br/> Width	74.4 mm<br/>Height	166.6 mm<br/>Thickness	8.3 mm  ");
+        
+        mainInfo.style.display=("block");
+        moreInfo.style.display=("block"); 
     }
 
  if (username=="mollyjacob1980@gmail.com"){
