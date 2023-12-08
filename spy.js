414ae584b4b8073d6dd7ec3e3a7718ff60f5fd80
trackmateai <136100891+trackmateai@users.noreply.github.com>
2023-12-08 17:56:16 +0200

    Create spy.js

---
+++ spy.js
@@ -0,0 +1,20 @@
+
+function socials(){
+    alert("This device does not support ZS-Unix engine,update device or contact manufacturer");   
+}
+
+
+ 
+function toHome(){
+               
+               var mainLay = document.getElementById("MainLay");
+               var splashLay = document.getElementById("SplashLay");
+
+               
+               mainLay.style.display=("block");
+               splashLay.style.display=("none");
+ }
+          
+
+
+
