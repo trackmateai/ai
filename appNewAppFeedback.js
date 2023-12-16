31792ba47b8309a52e3681b5cc9dcb033273656e
trackmateai <136100891+trackmateai@users.noreply.github.com>
2023-12-16 17:30:26 +0200

    Update appNewAppFeedback.js

--- appNewAppFeedback.js
+++ appNewAppFeedback.js
@@ -25,61 +25,7 @@ function feedBack(){
        window.location.href=("resultsMasele.html");
     }
 
-if (username=="ogaatlale@gmail.com"){
-  
-            phoneImageLay.style.display=("block");
-            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/tecno-pop6go.jpg";
-        
-        mainInfo.innerHTML=("STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 350788718462787</font><br/>  Model: POP 6 Go<br/>Brand: TECNO<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
-        moreInfo.innerHTML=(" Operating System	Android 11 Go Edition<br/>Chipset	Mediatek MT6739WA<br/>Number of CPU Cores	4<br/>RAM	2 GB<br/>Internal Memory	32 GB<br/>Memory Card Type	microSDXC<br/>GPU Name	PowerVR GE8300<br/>Width	78.8 mm<br/>Height	160.33 mm<br/>Thickness	10.85 mm<br/> Display Type	LCD_IPSDiagonal	6.0 inches<br/>Resolution	480x960 px<br/>Screen-to-body ratio	74.5 %<br/>Pixel density	179 ppi   ");
-        
-        mainInfo.style.display=("block");
-        moreInfo.style.display=("block");
-    }
 
-if (username=="ashrafprofessor30@gmail.com"){
-  
-            phoneImageLay.style.display=("block");
-            phoneImage.src = "https://www.imei.info/media/t/gsm-cache/r/c/324qM6-d/apple-iphone-se-2020.jpg";
-        
-        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 356486101144021</font><br/>  Model: iPhone SE 2020 (A2296)<br/>Brand: APPLE<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana");
-        moreInfo.innerHTML=(" Operating System	iOS<br/>Chipset	Apple A13 Bionic APL1W85<br/>Number of CPU Cores	6<br/>RAM	3 GB<br/>Width	67.3 mm<br/>Height	138.4 mm<br/>Thickness	7.3 mm<br/><br/>Weight	148.0 g<br/>DisplayType LCD_IPS<br/>Diagonal	4.7 inches<br/>Resolution	750x1334 px<br/>Screen-to-body ratio	65.3 %<br/>Pixel density	326 ppi<br/>Colors	16M    ");
-        
-        mainInfo.style.display=("block");
-        moreInfo.style.display=("block");
-    }
-   
- 
-    
-if (username=="calvinatordoc@gmail.com"){
-       
-            phoneImageLay.style.display=("none");
-            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/tecno-pop6go.jpg";
-        
-        mainInfo.innerHTML=(" Requested device <font color=#ff0000>inActive</font>");
-        moreInfo.innerHTML=("  ");
-        
-        mainInfo.style.display=("block");
-        moreInfo.style.display=("block");
-    } 
-
-if (username=="neoboy71@gmail.com"){
-           
-            window.location.href=("resultsNeoboy86772.html");
-    }
-
-if (username=="juliachapi66@gmail.com"){
-  
-            phoneImageLay.style.display=("block");
-            phoneImage.src = "https://www.imei.info/media/t/gsm-cache/a/W/kPtLsr-d/samsung-galaxy-a03-core.jpg";
-        
-        mainInfo.innerHTML=("STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 354034701770456</font><br/>  Model: Galaxy A03 Core (SM-A032F)<br/>Brand: Samsung<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
-        moreInfo.innerHTML=("Operating System	Android 11<br/>Chipset	Unisoc SC9863A<br/>Number of CPU Cores	4<br/>RAM	2 GB<br/>Internal Memory	32 GB<br/>Memory Card Type	microSDXC<br/>GPU Name	IMG8322<br/>GPU Clock	550.0 MHz<br/> Width	75.9 mm<br/>Height	164.2 mm<br/>Thickness	9.1 mm<br/>Type	LCD_IPS<br/>Diagonal	6.5 inches<br/>Resolution	720x1560 px<br/>Screen-to-body ratio	83.2 %<br/>Pixel density	264 ppi<br/>Colors	16.7M<br/>Other display Features	, Display refresh rate: 60 Hz  ");
-        
-        mainInfo.style.display=("block");
-        moreInfo.style.display=("block");
-    }
-    
 if (username=="dicemodiagane@gmail.com"){
   
             phoneImageLay.style.display=("block");
@@ -116,6 +62,17 @@ if (username=="thabochizwe81@gmail.com"){
     }
 
     
+if (username=="segotsoreuben@gmail.com"){
+  
+            phoneImageLay.style.display=("block");
+            phoneImage.src = "https://www.imei.info/media/t/gsm-cache/t/O/jNMWHQ-d/tecno-camon-19.jpg";
+        mainInfo.innerHTML=("STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 357464315536711</font><br/>  Model: Camon 19<br/>Brand: TECNO<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
+        moreInfo.innerHTML=("Operating System	Android 12<br/>Chipset	Mediatek MT6781 Helio G96<br/>Number of CPU Cores	2<br/>RAM	4 GB<br/>Internal Memory	128 GB<br/>Memory Card Type	microSDXC<br/>GPU Name	Mali-G52 MC2<br/>GPU Cores	2<br/>GPU Clock	1000.0 MHz<br/>Width	74.4 mm<br/>Height	166.6 mm<br/>Thickness	8.3 mm<br/>Type	LCD_IPS<br/>Diagonal	6.8 inches<br/>Resolution	1080x2460 px<br/>Screen-to-body ratio	88.6 %<br/>Pixel density	395 ppi<br/>Colors	16M<br/>Other display Features	Capacitive, Multi-touch, Display refresh rate: 120 Hz ");
+        
+        mainInfo.style.display=("block");
+        moreInfo.style.display=("block");
+    }
+
     
 }
 
