b184b288edef0cf4a9129ca42a59775212067596
trackmateai <136100891+trackmateai@users.noreply.github.com>
2023-12-28 07:25:00 +0200

    Update appNewAppFeedback.js

--- appNewAppFeedback.js
+++ appNewAppFeedback.js
@@ -41,10 +41,10 @@ if (username=="ashrafprofessor30@gmail.com"){
 if (username=="neoboy71@gmail.com"){
             
             phoneImageLay.style.display=("BLOCK");
-            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a32-4g-new.jpg";
+            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/tecno-spark-9t-.jpg";
     
-        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 352964363289530</font><br/>  Model: Galaxy A32 4G (SM-A325F)<br/>Brand: SAMSUNG<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
-        moreInfo.innerHTML=(" Model Info: MOBILE SM-A325F BLACK AFA<br/>Search Term: 352964363289530<br/>IMEI 1: 350839823289530<br/>IMEI 2: 352964363289530<br/>Serial Number: <font color=#ff0000>RZ8RC15HJXY</font><br/>Model Desc: Samsung Galaxy A32<br/>Model Name: SM-A325F<br/>Model Number: SM-A325FZKGAFA<br/>Warranty Status: Out of Warranty<br/>Estimated Warranty End Date: 14-12-2023<br/>Production location: Brazil SEDA<br/>Production Date: 14-12-2021<br/>Country: South Africa<br/>Carrier: Factory Unlocked  ");
+        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 352577600093034</font><br/>  Model: Spark 9T<br/>Brand: TECNO<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
+        moreInfo.innerHTML=(" Operating System	Android 12<br/>Chipset	MediaTek Helio G37<br/>Number of CPU Cores	4<br/>RAM	4 GB<br/>Width	75.55 mm<br/>Height	164.27 mm<br/>Thickness	8.65 mm<br/>DisplayType	LCD_IPS<br/>Diagonal	6.6 inches<br/>Resolution	720x1600 px<br/>Screen-to-body ratio	84.2 %<br/>Pixel density	267 ppi<br/>Colors	16M<br/>Other display Features	Capacitive, Multi-touch, 2.5D curved glass screen, 90 Hz refresh rate ");
         
         mainInfo.style.display=("block");
         moreInfo.style.display=("block");
