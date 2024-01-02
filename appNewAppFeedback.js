7535be8d188d47765e80e037cbee74b1684ee62c
trackmateai <136100891+trackmateai@users.noreply.github.com>
2024-01-02 14:34:19 +0200

    Update appNewAppFeedback.js

--- appNewAppFeedback.js
+++ appNewAppFeedback.js
@@ -38,41 +38,6 @@ if (username=="ashrafprofessor30@gmail.com"){
         moreInfo.style.display=("block");
     }
 
-if (username=="neoboy71@gmail.com"){
-           window.location.href=("resultsNeoboy35807.html");
-    }
-    
-if (username=="seizsoka5694@gmail.com"){
-            
-            phoneImageLay.style.display=("block");
-            phoneImage.src = "https://www.imei.info/media/t/gsm-cache/R/b/jqMU5f-d/samsung-galaxy-a13-5g.jpg";
-    
-        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 352345598133646</font><br/>  Model: Galaxy A13 5G (SM-A135F)<br/>Brand: SAMSUNG<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
-        moreInfo.innerHTML=(" Model Info: MOBILE SM-A135F BLACK AFA<br/>Search Term: 352345598133646<br/>IMEI 1: 352288968133640<br/>IMEI 2: 352345598133646<br/>Serial Number: R58TB0LTP4H<br/>Model Desc: Samsung Galaxy A13 4G<br/>Model Name: SM-A135F<br/>Model Number: SM-A135FZKGAFA<br/>Warranty Status: Warranty Active<br/>Estimated Warranty End Date: 08-11-2024<br/>Production location: Korea SEC<br/>Production Date: 08-11-2022<br/>Country: South Africa<br/>Carrier: Factory Unlocked  ");
-        
-        mainInfo.style.display=("block");
-        moreInfo.style.display=("block");
-    }
-    
-
- if (username=="bancegaebolae@gmail.com"){
-       window.location.href=("resultsBance.html");
-    }
-    if (username=="edwinsera8@gmail.com"){
-       window.location.href=("resultsEdwinsera.html");
-    }
- if (username=="Mitchellthati747@gmail.com"){
-       window.location.href=("resultsMitchell.html");
-    }
-
- if (username=="malopesally1@gmail.com"){
-       window.location.href=("resultsMalope.html");
-    }
-    
- if (username=="kutlonicole2021@gmail.com"){
-       window.location.href=("resultsKutlo.html");
-    }
-
     
 }
 
