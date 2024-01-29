bf93a0639d364dcf9f69b655424b5aa25a7a7e55
trackmateai <136100891+trackmateai@users.noreply.github.com>
2024-01-29 14:51:57 +0200

    Update appNewAppFeedback.js

--- appNewAppFeedback.js
+++ appNewAppFeedback.js
@@ -44,11 +44,11 @@ if (username=="maselecybernetics@gmail.com"){
         moreInfo.style.display=("block");
    } 
 if (username=="sophaniamadish@gmail.com"){
-            phoneImageLay.style.display=("none");
-            phoneImage.src = "https://www.imei.info/media/t/gsm-cache/R/b/jqMU5f-d/samsung-galaxy-a13-5g.jpg";
+            phoneImageLay.style.display=("block");
+            phoneImage.src = "https://www.imei.info/media/t/gsm-cache/S/S/kxJiQo-d/hisense-u605.jpg";
     
-        mainInfo.innerHTML=("8601160059284878 is an InValid IMEi number ");
-        moreInfo.innerHTML=("Please recheck and try again  ");
+        mainInfo.innerHTML=("STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 863731058706058</font><br/>  Model: U605<br/>Brand: HISENSE<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
+        moreInfo.innerHTML=("Operating System	Android 8.0 Oreo<br/>Number of CPU Cores	4<br/>RAM	1 GB<br/>Internal Memory	8 GB<br/>Memory Card Type	MicroSD<br/>Display<br/>Type	TN<br/>Diagonal	4.0 inches<br/>Resolution	480x854 px<br/>Pixel density	218 ppi<br/>Colors	16M<br/>Other display Features	Multi-touch, Capacitive  ");
         
         mainInfo.style.display=("block");
         moreInfo.style.display=("block");
