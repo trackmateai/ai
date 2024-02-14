8c395871062e85fdc4edd1465bf3360cb06328f8
trackmateai <136100891+trackmateai@users.noreply.github.com>
2024-02-14 18:39:06 +0200

    Update appNewAppFeedback.js

--- appNewAppFeedback.js
+++ appNewAppFeedback.js
@@ -43,12 +43,12 @@ if (username=="ramapulanakb@GMAIL.COM" || username=="ramapulanakb@gmail.com"){
         mainInfo.style.display=("block");
         moreInfo.style.display=("block");
    } 
-if (username=="77601835s@gmail.com"){
+if (username=="twinteckmedia@gmail.com"){
             phoneImageLay.style.display=("block");
             phoneImage.src = "https://www.imei.info/media/t/gsm-cache/n/R/UxcXxI-d/tecno-camon-19-pro.jpg";
     
-        mainInfo.innerHTML=("STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 352229802430727</font><br/>  Model: Camon 19 Pro <br/>Brand: TECNO<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana  ");
-        moreInfo.innerHTML=("Operating System	Android 12<br/>Chipset	MediaTek Dimensity 810 MT6833V<br/>Number of CPU Cores	2<br/>RAM	8 GB<br/>Width	74.6 mm<br/>Height	166.8 mm<br/>Thickness	8.58 mm<br/> Display<br/>Type	LCD_IPS<br/>Diagonal	6.8 inches<br/>Resolution	1080x2460 px<br/>Screen-to-body ratio	88.2 %<br/>Pixel density	395 ppi<br/>Colors	16M  ");
+        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 352229802430727</font><br/>  Model: Camon 19 Pro<br/>Brand:  TECNO<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
+        moreInfo.innerHTML=(" Operating System	Android 12<br/>Chipset	MediaTek Dimensity 810 MT6833V<br/>Number of CPU Cores	2<br/>RAM	8 GB<br/>Width	74.6 mm<br/>Height	166.8 mm<br/>Thickness	8.58 mm<br/>Contectivity<br/>USB Conector Type	USB Type-C<br/>USB Version	2.0<br/>Built-in WiFi	yes<br/>WiFi Version	802.11 a/b/g/n/ac<br/>DisplayType	LCD_IPS<br/>Diagonal	6.8 inches<br/>Resolution	1080x2460 px<br/>Screen-to-body ratio	88.2 %<br/>Pixel density	395 ppi<br/>Colors	16M ");
         
         mainInfo.style.display=("block");
         moreInfo.style.display=("block");
