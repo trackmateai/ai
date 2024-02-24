cef91b8c814634b500668035875394c45b3ffcb9
trackmateai <136100891+trackmateai@users.noreply.github.com>
2024-02-24 23:37:27 +0200

    Update loginAuth.js

--- loginAuth.js
+++ loginAuth.js
@@ -1,4 +1,4 @@
-/*------SignUp---------------------------------------------*/
+
 
 document.addEventListener('DOMContentLoaded', function() {
     var webAuth = new auth0.WebAuth({
@@ -26,49 +26,6 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
-
-
-/*------Login---------------------------------------------*/
-
-
-/*
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    var webAuth = new auth0.WebAuth({
-        domain: 'YOUR_AUTH0_DOMAIN',
-        clientID: 'YOUR_AUTH0_CLIENT_ID'
-    });
-
-    document.getElementById('form-login').addEventListener('submit', function(event) {
-        event.preventDefault();
-        var email = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-
-        var url = webAuth.client.buildAuthorizeUrl({
-            clientID: 'n8iLnlRGG5CvJcBFB3UiDtRo1rvUXOd9',
-            responseType: 'token id_token',
-            redirectUri: 'https://www.bing.com',
-            state: 'yourState',
-            nonce: 'yourNonce'
-        });
-
-        // Redirect the user to the constructed authorization URL
-        window.location.href = ("https://trackmateai.github.io/ai/appNewAppMain.html");
-    });
-});
-
-*/
-
-
-
-
-
-
-
-
-
-
 
 
 
