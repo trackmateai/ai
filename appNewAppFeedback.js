28dc626925b211f071413ce31b68faa1c64f3c4b
trackmateai <136100891+trackmateai@users.noreply.github.com>
2024-02-27 11:44:54 +0200

    Update appNewAppFeedback.js

--- appNewAppFeedback.js
+++ appNewAppFeedback.js
@@ -19,10 +19,8 @@ function feedBack(){
                 phoneImageLay= document.getElementById("imageLay")
                 phoneImage= document.getElementById("phoneImage")
       
-  if (username=="maitumeloonamile@gmail.com"){
-           window.location.href=("resultsMaitumelo.html");
-  }   
-if (username=="atasebeke@gmail.com"){
+
+    if (username=="atasebeke@gmail.com"){
             phoneImageLay.style.display=("block");
             phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12.jpg";
     
@@ -32,59 +30,8 @@ if (username=="atasebeke@gmail.com"){
         mainInfo.style.display=("block");
         moreInfo.style.display=("block");
 }   
-if (username=="monagengkeneiloe@gmail.com"){
-            phoneImageLay.style.display=("block");
-            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a12-sm-a125.jpg";
-    
-        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 359573263186101</font><br/>  Model: Galaxy A12 (SM-A125F)<br/>Brand: Samsung<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
-        moreInfo.innerHTML=(" Model Info: MOBILE SM-A125F/DS BLUE AFA<br/>Search Term: 359573263186101<br/>IMEI 1: 359573263186101<br/>IMEI 2: 359613633186108<br/>Serial Number:<font color=#ff0000> R58R54SNRWY</font><br/>Model Desc: Samsung Galaxy A12<br/>Model Name: SM-A125F/DS<br/>Model Number: SM-A125FZBGAFA<br/>Warranty Status: Out of Warranty<br/>Estimated Warranty End Date: 27-05-2023<br/>Production location: Korea SEC<br/>Production Date: 27-05-2021<br/>Country: Botswana<br/>Carrier: Factory Unlocked ");
-        
-        mainInfo.style.display=("block");
-        moreInfo.style.display=("block");
-   } 
-if (username=="phetsookay@gmail.com"){
-            phoneImageLay.style.display=("block");
-            phoneImage.src = "https://www.imei.info/media/t/gsm-cache/k/O/MCpIfl-d/samsung-galaxy-a24.jpg";
-    
-        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 354916449643922</font><br/>  Model: Galaxy A24 (SM-A245F)<br/>Brand: SAMSUNG<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
-        moreInfo.innerHTML=(" Model Info: MOBILE SM-A245F BLACK MEA<br/>Search Term: 354916449643922<br/>IMEI 1: 354916449643922<br/>Serial Number: <font color=#ff0000>RZ8W80DXD2Z</font><br/>Model Desc: Samsung Korea Galaxy A24<br/>Model Name: SM-A245F<br/>Model Number: SM-A245FZKUMEA<br/>Warranty Status: Warranty Active<br/>Estimated Warranty End Date: 22-08-2025<br/>Production location: Brazil SEDA<br/>Production Date: 22-08-2023<br/>Country: Botswana<br/>Carrier: Factory Unlocked ");
-        
-        mainInfo.style.display=("block");
-        moreInfo.style.display=("block");
-   } 
 
-if (username=="thuto2018th@gmail.com"){
-            phoneImageLay.style.display=("block");
-            phoneImage.src = "https://www.imei.info/media/t/gsm-cache/u/Z/dd2mZE-d/huawei-p40-lite.jpg";
     
-        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 867991056018411</font><br/>  Model:  P40 lite (JNY-LX1) <br/>Brand: HUAWEI<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana  ");
-        moreInfo.innerHTML=(" Description Full: HUAWEI P40 lite 6GB+128GB Midnight Black Dual Card Open Market Ver. NEEU EU Standard<br/>Model Code: Jenny-L21A<br/>IMEI: 867991056018411<br/>S/N: <font color=#ff0000>EPHUT21309006341</font><br/>Item Code: 51095CJV<br/>Offer Code: OFFE00148682<br/>Country Code: +267<br/>Country Name: Botswana<br/>Warranty Status: Warranty Active<br/>Warranty Start:<br/>Warranty Expire: 2024/7/4 ");
-        
-        mainInfo.style.display=("block");
-        moreInfo.style.display=("block");
-   }
-
-if (username=="Ramapulanakb@GMAIL.COM"){
-            phoneImageLay.style.display=("block");
-            phoneImage.src = "https://www.imei.info/media/t/gsm-cache/X/H/BpkjP1-d/tecno-pop-7-pro.jpg";
-    
-        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 359696716993243</font><br/>  Model: Pop 7 Pro<br/>Brand: TECNO<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana  ");
-        moreInfo.innerHTML=(" Operating System	Android 12<br/>Chipset	Mediatek MT6761V Helio A22<br/>Number of CPU Cores	4<br/>RAM	3 GB<br/>Width	75.5 mm<br/>Height	163.9 mm<br/>Thickness	8.9 mm<br/>Weight	191.0 g<br/> DisplayType	LCD_IPS<br/>Diagonal	6.56 inches<br/>Resolution	720x1600 px<br/>Screen-to-body ratio	83.5 %<br/>Pixel density	267 ppi<br/>Colors	16M<br/>Other display Features	Capacitive, Multi-touch,");
-        
-        mainInfo.style.display=("block");
-        moreInfo.style.display=("block");
-   }
-
-if (username=="ashrafprofessor30@gmail.com"){
-            phoneImageLay.style.display=("block");
-            phoneImage.src = "https://www.imei.info/media/t/gsm-cache/T/A/9jTExH-d/xiaomi-mi-note-10.jpg";
-    
-        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 864348055210114</font><br/>  Model: Mi Note 10 (M2101K7AG)<br/>Brand: XIAOMI<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana  ");
-        moreInfo.innerHTML=(" Operating System	Android 9.0 Pie<br/>Chipset	Qualcomm Snapdragon 730G<br/>Number of CPU Cores	2<br/>RAM	6 GB<br/>Width	74.2 mm<br/>Height	157.8 mm<br/>Thickness	9.67 mm<br/>Weight	208.0 g<br/>DisplayType	AMOLED<br/>Diagonal	6.47 inches<br/>Resolution	1080x2340 px<br/>Screen-to-body ratio	87.7 %<br/>Pixel density	398 ppi<br/>Colors	16M<br/>Other display Features	Capacitive, Multi-touch, Scratch resistant ");
-        
-        mainInfo.style.display=("block");
-        moreInfo.style.display=("block");
-   }
     
             
 }
