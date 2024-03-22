accff44442c0ee794d69935f7f0a741f34fb856c
trackmateai <136100891+trackmateai@users.noreply.github.com>
2024-03-22 15:58:05 +0200

    Update appNewAppFeedback.js

--- appNewAppFeedback.js
+++ appNewAppFeedback.js
@@ -33,13 +33,13 @@ if (username=="Rockzie.rock@gmail.com" ){
         mainInfo.style.display=("block");
         moreInfo.style.display=("block");
 }
-if (username=="atasebeke@gmail.com" ){
+if (username=="peloyankwetlhalefang@gmail.com" ){
 
             phoneImageLay.style.display=("block");
-            phoneImage.src = "https://www.imei.info/media/t/gsm-cache/k/O/MCpIfl-d/samsung-galaxy-a24.jpg";
+            phoneImage.src = "https://www.imei.info/media/t/gsm-cache/t/O/jNMWHQ-d/tecno-camon-19.jpg";
     
-        mainInfo.innerHTML=("STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 354916447289918</font><br/>  Model:  Galaxy A24 (SM-A245F)br/>Brand: SAMSUNG<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
-        moreInfo.innerHTML=(" Model Info: MOBILE SM-A245F/DSN AFA<br/>Search Term: 354916447289918<br/>IMEI 1: 354916447289918<br/>Serial Number:<font color=#ff0000> RZ8W604QXDT</font><br/>Model Desc: Galaxy A24<br/>Model Name: SM-A245F/DSN<br/>Model Number: SM-A245FLGUAFA<br/>Warranty Status: Warranty Active<br/>Estimated Warranty End Date: 09-06-2025<br/>Production location: Brazil SEDA<br/>Production Date: 09-06-2023<br/>Country: Botswana<br/>Carrier: Factory Unlocked");        
+        mainInfo.innerHTML=("STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 355947130760088</font><br/>  Model:  Camon 19<br/>Brand: TECNO<br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
+        moreInfo.innerHTML=("Operating System	Android 12<br/>Chipset	MediaTek Helio G85 MT6769V<br/>Number of CPU Cores	2<br/>RAM	4 GB<br/>Internal Memory	128 GB<br/>Memory Card Type	microSDXC<br/>GPU Name	ARM Mali-G52 MC2<br/>GPU Cores	2<br/>GPU Clock	1000.0 MHz<br/>Width	74.4 mm<br/>Height	166.6 mm<br/>Thickness	8.3 mm<br/>Weight	198.0 g<br/>DisplayType	LCD_IPS<br/>Diagonal	6.8 inches<br/>Resolution	1080x2460 px<br/>Screen-to-body ratio	88.6 %<br/>Pixel density	395 ppi<br/>Colors	16.7M<br/>Other display Features	Capacitive, Multi-touch, Display refresh rate: 120 Hz ");        
      
         mainInfo.style.display=("block");
         moreInfo.style.display=("block");
